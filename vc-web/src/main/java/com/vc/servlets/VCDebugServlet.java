97e989b570dc77bf9e32503da9be2ef194459e62
cmparish <cmparish@gmail.com>
2017-03-27 23:31:08 -0500

    added debug servlet for testing

---
+++ vc-web/src/main/java/com/vc/servlets/VCDebugServlet.java
@@ -0,0 +1,88 @@
+package com.vc.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.Enumeration;
+
+/**
+ * Created by cmparish on 3/27/17.
+ */
+public class VCDebugServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        process(request, response);
+
+    }
+
+    protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+
+        String pathInfo = request.getPathInfo();
+        String queryString = request.getQueryString();
+        String requestType = request.getMethod();
+        String uriString = request.getRequestURI();
+        
+
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        String requestBody = buffer.toString();
+        if (requestBody == null ) {requestBody = ""; }
+
+        System.out.println("\n\n ******************************* ");
+        System.out.println("PathInfo: " + pathInfo);
+        System.out.println("queryString: " + queryString);
+        System.out.println("requestType: " + requestType);
+        System.out.println("requestBody: " + requestBody);
+        System.out.println("uriString: " + uriString);
+        System.out.println("+++ Headers +++");
+        System.out.println(getHeaderValues(request,"\n"));
+        System.out.println("+++ Headers +++");
+        System.out.println("\n\n ******************************* ");
+
+
+        // Set response content type
+        response.setContentType("text/html");
+        out.println("{}");
+        
+    }
+
+    private String getHeaderValues(HttpServletRequest request, String newLine){
+        Enumeration<String> headerEnum = request.getHeaderNames();
+        StringBuffer buffer = new StringBuffer();
+        while (headerEnum.hasMoreElements()) {
+            String headerName = headerEnum.nextElement();
+            String headerValue = request.getHeader(headerName);
+            buffer.append(headerName).append(":").append(headerValue).append(newLine);
+        }
+
+        return buffer.toString();
+    }
+}
